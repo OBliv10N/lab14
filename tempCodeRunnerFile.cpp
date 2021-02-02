ad6fd3dafea82d4ae957433b2fe9a01db543f6e2
OBlilly <mekirito@hotmail.com>
2021-02-02 15:37:55 +0700

    update

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -7,11 +7,11 @@ int main(){
 	double b[] = {5.5,2.1,13,-7,6.9,0,25,11.1,-4}; 
 	
 	int pa = *max_element(a,a+5); 
-	cout << "Address of the highest value in array A is " << pa << "\n";
+	cout << "Address of the highest value in array A is " << &pa << "\n";
 	cout << "The highest value in array A is "<< pa << "\n";
 	
 	double pb = *max_element(b,b+9); 
-	cout << "Address of the highest value in array B is " << pb << "\n";
+	cout << "Address of the highest value in array B is " << &pb << "\n";
 	cout << "The highest value in array B is "<< pb << "\n";
 
 	return 0;
